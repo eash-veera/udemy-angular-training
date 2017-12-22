dfb71ba13b62ef129f9ef32ba32753acf0b956ac
eash-veera <eshwaranveerabahu.biz@gmail.com>
2017-12-22 15:55:36 +0530

    experimenting with different selector types for components

--- bootstrap-app/src/app/servers/servers.component.ts
+++ bootstrap-app/src/app/servers/servers.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // selector: '[app-servers]', --> selector by attribute
+  // selector: '.app-servers', --> selector by class
+  // below selector is the default way of selecting a component
   selector: 'app-servers',
   templateUrl:'./servers.component.html',
   styleUrls: ['/servers.component.css']
